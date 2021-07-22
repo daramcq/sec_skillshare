8fd947bf6ccba280eb33af6363f66621e3166f0f
Dara McHugh <dmchugh@zendesk.com>
2021-07-22 23:03:59 +0100

    Extra template to show iteration

--- template_app/basic.py
+++ template_app/basic.py
@@ -10,5 +10,11 @@ def index():
     return render_template('basic.html', name=name)
 
 
+@app.route('/goodnight')
+def goodnight():
+    things = ['brush', 'bowl full of mush', 'old lady whispering hush']
+    return render_template('goodnight.html', things=things)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
